6153a1be167bbf764cce7554577b11f6ebce050c
yaopeng-coder <m201973311@hust.edu.cn>
2019-12-01 14:16:19 +0800

    hystrix

---
+++ src/main/java/cn/hust/highconcurrent/hystrix/HystrixController2.java
@@ -0,0 +1,54 @@
+package cn.hust.highconcurrent.hystrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * @program: high-concurrent
+ * @author: yaopeng
+ * @create: 2019-12-01 13:32
+ **/
+@Slf4j
+@Controller
+@RequestMapping("/hystrix2")
+@DefaultProperties(defaultFallback = "defaultFail")
+public class HystrixController2 {
+
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+    })
+    @RequestMapping("/test1")
+    @ResponseBody
+    public String test1() throws Exception {
+        Thread.sleep(1000);
+        return "test1";
+    }
+
+    @HystrixCommand(commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")},
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "30"),
+                    @HystrixProperty(name = "maxQueueSize", value = "101"),
+                    @HystrixProperty(name = "keepAliveTimeMinutes", value = "2"),
+                    @HystrixProperty(name = "queueSizeRejectionThreshold", value = "15"),
+                    @HystrixProperty(name = "metrics.rollingStats.numBuckets", value = "12"),
+                    @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "1440")
+            })
+    @RequestMapping("/test2")
+    @ResponseBody
+    public String test2() throws Exception {
+        Thread.sleep(1000);
+        return "test2";
+    }
+
+    private String defaultFail() {
+        log.warn("default fail");
+        return "default fail";
+    }
+}
